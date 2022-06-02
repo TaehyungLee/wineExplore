c1124d5b414f7e6fe419867d5903ad87b930d698
KBSYS <thlee@kb-sys.co.kr>
2022-06-02 16:55:30 +0900

    test

--- WineExplore/ViewController.swift
+++ WineExplore/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     let viewModel = ViewModel()
     var disposeBag = DisposeBag()
     
+    // commit test
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
